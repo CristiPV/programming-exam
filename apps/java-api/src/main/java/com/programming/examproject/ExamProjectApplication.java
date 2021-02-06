81f6ae4edd66a56a7f191787fcffb8d441c20548
Cristian <cristi.valentin29@gmail.com>
2021-02-06 18:19:42 +0100

    fix: 🐛 Fix git cz
    finally
    
    BREAKING CHANGE: 🧨 none
    
    ✅ Closes: none

--- apps/java-api/src/main/java/com/programming/examproject/ExamProjectApplication.java
+++ apps/java-api/src/main/java/com/programming/examproject/ExamProjectApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ExamProjectApplication
 {
-
+    // testing commits - delete later
     public static void main(String[] args)
     {
         SpringApplication.run(ExamProjectApplication.class, args);
